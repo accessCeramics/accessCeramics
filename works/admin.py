72d96ecdc80ae791abf54520866fa8b5f4431d48
Nick Budak <nbudak@princeton.edu>
2018-04-13 16:11:51 -0400

    add work models to admin

--- works/admin.py
+++ works/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from works.models import Material, Technique, Work, WorkType
+
+class WorkAdmin(admin.ModelAdmin):
+    pass
+
+class TechniqueAdmin(admin.ModelAdmin):
+    pass
+    
+class MaterialAdmin(admin.ModelAdmin):
+    pass
+
+class WorkTypeAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Work, WorkAdmin)
+admin.site.register(Technique, TechniqueAdmin)
+admin.site.register(Material, MaterialAdmin)
+admin.site.register(WorkType, WorkTypeAdmin)
